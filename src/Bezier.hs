39ef077dfd3af15a8f0e48cbc493051faced12b7
Aaron Robson <ARobsonSE@hotmail.co.uk>
2016-05-18 18:53:54 +0100

    fluxions definition back again.

--- src/Bezier.hs
+++ src/Bezier.hs
@@ -26,6 +26,7 @@ yPoint = snd
 type T = Double
 
 fluxions :: [T]
+fluxions = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 
 difference1d :: Point1d -> Point1d -> Point1d
 difference1d p1 p2 = p2 - p1
